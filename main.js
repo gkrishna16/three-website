35554082cff54c0687670eecf9e555dd29692177
Gopal Krishan <gopalkbks@Gopals-MacBook-Pro.local>
2021-08-08 21:14:20 -0400

    size js fix

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 window.addEventListener("resize", function () {
   "use strict";
-  if (window.matchMedia("(min-width: 500px)").matches) {
+  if (window.matchMedia("(min-width: 600px)").matches) {
     console.log("Screen width is at least 500px");
     window.location.reload();
   } else {
